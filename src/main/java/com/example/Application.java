d422b1bbc62a41fb23b2ff60c7546daf57d9775d
Svata Dedic <svatopluk.dedic@oracle.com>
2021-03-02 08:36:19 +0100

    Exhibit mn:run commandline parsing bug.

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,6 +5,10 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        System.err.println("Passed parameters:");
+        for (String s : args) {
+            System.err.println("- \"" + s + '"');
+        }
         Micronaut.run(Application.class, args);
     }
 }
